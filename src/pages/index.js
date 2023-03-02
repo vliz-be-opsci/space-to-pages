357ce56c8cfe2e30f17a31b8700b371e987e0642
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2023-03-02 13:20:01 +0100

    first push

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import "../styles/index.css"
+
+const IndexPage = () => {
+  return (
+    <main >
+      test
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
