4457fa6418c073cf0d420adf47f5f5f79bfc4207
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2023-03-02 13:38:03 +0100

    deleted site metadata as a test to see if the site still builds

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `bon-template-website`
-  },
   pathPrefix: "/bon-template-website",
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
