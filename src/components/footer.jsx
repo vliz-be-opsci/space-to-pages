2dd5af4cdfd12f0cb4cc99ed56931a10873e78c4
cedricd <cedricdecruw@gmail.com>
2023-10-09 16:08:54 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,17 +12,17 @@ export const Footer = (props) => {
                 <a href="https://github.com/vliz-be-opsci/https://github.com/vliz-be-opsci/space-to-pages"> space-to-pages </a> 
                 from
                 <a href="https://open-science.vliz.be/" target="_blank">
-                    <img src="https://open-science.vliz.be/img/VLIZ_LOGO.svg" alt="@vliz-be-opsci" className="footer_logo"/>
+                    <img src="https://open-science.vliz.be/common-assets/img/VLIZ_LOGO.svg" alt="@vliz-be-opsci" className="footer_logo"/>
                 </a>
                 with data 
                 <a href={"https://github.com/"+process.env.REACT_APP_GH_REPO} target="_blank">
-                    <img src="https://open-science.vliz.be/img/github-logo.svg" alt="data-location" className="footer_logo"/>
+                    <img src="https://open-science.vliz.be/common-assets/img/github-logo.svg" alt="data-location" className="footer_logo"/>
                 </a>
             </p>
             <p>
                 as 
                 <a href="./metadata.ttl">
-                    <img src="https://open-science.vliz.be/img/rdf-logo.svg" alt="metadata.ttl" className="footer_logo"/>
+                    <img src="https://open-science.vliz.be/common-assets/img/rdf-logo.svg" alt="metadata.ttl" className="footer_logo"/>
                 </a>
             </p>
       </div>
