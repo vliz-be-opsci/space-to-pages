fae4b846211a2376bc1fb0040c1f97609a42512e
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-11-20 15:00:04 +0100

    Update entrypoint script to use sema-subyt command instead of pysubyt

--- entrypoint.sh
+++ entrypoint.sh
@@ -117,7 +117,7 @@ echo "removing the old output folder contents if they exist"
 rm -rf ./outputs/*
 #then run the pysubyt command
 echo "running the pysubyt commands"
-python -m pysubyt -t ./templates/  \
+sema-subyt -t ./templates/  \
        -s contact ../src/data/contacts.json \
        -s main ../src/data/main_data.json \
        -s publications ../src/data/publications.json \
