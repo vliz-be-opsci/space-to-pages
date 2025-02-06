fea7c8908fcc54770d5610851800e03cfb1f0e2d
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2025-02-06 09:37:26 +0100

    Add support for books data in navigation entry

--- src/navigation_entry.js
+++ src/navigation_entry.js
@@ -4,11 +4,13 @@ import { Navigation } from "./components/navigation";
 import MainData from "./data/main_data.json";
 import CratesData from "./data/project_crates.json";
 import ProfilesData from "./data/project_profiles.json";
+import BooksData from "./data/books.json";
 
 const data = {
   Header: MainData,
   Crates: CratesData,
   Profiles: ProfilesData,
+  Books: BooksData,
 };
 
 ReactDOM.render(
@@ -16,6 +18,7 @@ ReactDOM.render(
     data={data.Header}
     crates={data.Crates}
     profiles={data.Profiles}
+    books={data.Books}
   />,
   document.getElementById("navigation-root")
 );
